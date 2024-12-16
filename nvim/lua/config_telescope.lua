2382e4b7b5c5204ade80140c55ebe93b3f581345
ralex2304 <ralex2304@gmail.com>
2024-12-16 07:27:40 +0300

    [neovim] telescope file browser opening in current dir

--- nvim/lua/config_telescope.lua
+++ nvim/lua/config_telescope.lua
@@ -84,6 +84,6 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Telescope find git files' })
-vim.keymap.set("n", "<leader>fi", ":Telescope file_browser<CR>")
+vim.keymap.set("n", "<leader>fi", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
 vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
 
