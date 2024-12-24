1fd4df3ae3c1b0f6d9d1d57632aba3051b436e91
ralex2304 <ralex2304@gmail.com>
2024-12-24 17:34:38 +0300

    [neovim] scrolloff added

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -17,6 +17,7 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+vim.opt.scrolloff = 5
 
 local group_cdpwd = vim.api.nvim_create_augroup("group_cdpwd", { clear = true })
 vim.api.nvim_create_autocmd("VimEnter", {
