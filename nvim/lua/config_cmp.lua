aba13238856eb957b6063d8cf8290bbddeffc2af
ralex2304 <ralex2304@gmail.com>
2024-12-26 02:31:30 +0300

    [neovim] latex completion added

--- nvim/lua/config_cmp.lua
+++ nvim/lua/config_cmp.lua
@@ -55,7 +55,8 @@ cmp.setup({
     -- { name = 'luasnip' }, -- For luasnip users.
     -- { name = 'ultisnips' }, -- For ultisnips users.
     -- { name = 'snippy' }, -- For snippy users.
-  }, {
+    { name = 'latex_symbols'},  
+}, {
     { name = 'buffer' },
   })
 })
