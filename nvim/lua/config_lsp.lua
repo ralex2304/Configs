c6f44b7b7aa56fa1d1d493bf8406a4602e7fa437
ralex2304 <ralex2304@gmail.com>
2025-03-23 10:03:39 +0300

    [neovim ]added GLSL syntax highlight and LSP

--- nvim/lua/config_lsp.lua
+++ nvim/lua/config_lsp.lua
@@ -15,7 +15,7 @@ vim.keymap.set('n', '<leader>so', require('telescope.builtin').lsp_references, a
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-local servers = { 'clangd', 'pyright', 'asm_lsp', 'cmake'}
+local servers = { 'clangd', 'pyright', 'asm_lsp', 'cmake', 'glsl_analyzer'}
 for _, lsp in ipairs(servers) do
 	require('lspconfig')[lsp].setup {
 		capabilities = capabilities,
@@ -35,6 +35,8 @@ require('lspconfig').veridian.setup {
 vim.filetype.add({
     extension = {
         nasm = 'asm',
+        vs = 'glsl',
+        fs = 'glsl'
     },
     filename = {
         ['CMakeLists.txt'] = 'cmake',
